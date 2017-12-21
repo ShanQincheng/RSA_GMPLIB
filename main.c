7f4454191762de5aaa56ecc1add69ed71e111da4
ShanQincheng <ipadzl@outlook.com>
2017-12-21 20:29:10 +0800

    add comments for codes

--- main.c
+++ main.c
@@ -4,103 +4,72 @@
 #include "random_number.h"
 #include "miller_rabin_primality_test.h"
 
-
 int main(void)
 {
   mpz_t p; // random COMPLEX bit prime p
-  mpz_t q; // random COMPLEX bit prime q
+  mpz_t q; // random COMPLEX bit prime q, q != p
   mpz_t n; // by the equation n = pq 
   mpz_t _n; // by equation _n = (p - 1)(q - 1)
-  //unsigned long int d;
-  mpz_t d;
+  mpz_t d;  // d is the multiplicative inverse of e, modulo _n; we can use Extend_EUCLID algorithm to solve this problem
   unsigned long int e; // a small odd integer e
   mpz_t limitNum; // set as random type or random number max limit
   mp_bitcnt_t bit_index = 0;  // the least significant bit is number 0 for function int mpz_tstbit()
-  struct PublicKeyPair
+  struct PublicKeyPair  // rsa public key pair struct
   { 
     unsigned long int e;
     mpz_t n;
   };
   struct PublicKeyPair public_key_pair;
   
-  struct PrivateKeyPair
+  struct PrivateKeyPair  // rsa private key pair struct
   {
-    //unsigned long int d;
     mpz_t d;
     mpz_t n;
   };
   struct PrivateKeyPair private_key_pair;
  
-  mpz_inits(limitNum, d, NULL);
- // mpz_init2(d, COMPLEX);
+  mpz_inits(limitNum, d, NULL);  // use mpz_inits or mpz_init2 function to initialize mpz_t type parameters before use them
   mpz_init2(p, COMPLEX);
   mpz_init2(q, COMPLEX);
-  bit_index = 2 * COMPLEX;
-  mpz_init2(n, bit_index);
-  mpz_init2(_n, bit_index);
-  bit_index = 0;
-  /*
-     Random COMPLEX bit p
-  */
- // mpz_set_ui(limitNum, 0);
- // RandomMpz_t(&p, limitNum);
-  Select_p(&p);
-  if(mpz_tstbit(p, bit_index))
-  {
-    while( MILLER_RABIN(p) ) mpz_add_ui(p, p, 2);
-  }else {
-    mpz_add_ui(p, p, 1);
-    while( MILLER_RABIN(p) ) mpz_add_ui(p, p, 2);
-  }
-  /*
-    Random COMPLEX bit q
-  */
-  //RandomMpz_t(&q, limitNum);
-  Select_q(&q);
-  if(mpz_tstbit(q, bit_index))
-  {
-    while( MILLER_RABIN(q) ) mpz_add_ui(q, q, 2);
-  }else {
-    mpz_add_ui(q, q, 1);
-    while( MILLER_RABIN(q) ) mpz_add_ui(q, q, 2);
-  }
+  bit_index = 2 * COMPLEX; // x bits number multiple y bits number , the result will be a (x+y) bits number
+  mpz_init2(n, bit_index); 
+  mpz_init2(_n, bit_index); // the same reason to n
+  bit_index = 0; // recovery bit_index to 0
+  
+  Select_p(&p);  // use random mpz_z type number algorithm to choose p
+  Select_q(&q);  // the sam reason to p
   gmp_printf("p == %Zd\n", p);
   gmp_printf("q == %Zd\n", q);
+
   Select_n(p, q, &n);  // n = pq
   gmp_printf("n (p * q) == %Zd\n", n);
+
   Select_Euler_n(p, q, &_n); // Euler n = (p-1)(q-1)
   gmp_printf("_n (p-1)*(q-1) == %Zd\n", _n);
+
   Select_e(&e, _n); //  gcd(_n, e)
   mpz_init(public_key_pair.n);
   mpz_set(public_key_pair.n, n); // P = (e. n) as RSA public key
   public_key_pair.e = e;
+  gmp_printf("public_key_n is %Zd\n", public_key_pair.n);
+  printf("public_key_e is %lu\n", public_key_pair.e);
 
-  Select_d(e, _n, &d);
-  //Select_d(_n, e, &d);
-  //unsigned long int ddd = 0; 
-  //Select_d_simple(e, _n, &ddd);
-  //mpz_set_ui(d, ddd);
-   
- // private_key_pair.d = d;
+  Select_d(e, _n, &d);  // use EXTEND EUCLID algorithm choose the unique d
   mpz_init(private_key_pair.d);
   mpz_set(private_key_pair.d, d);
   mpz_init(private_key_pair.n);
   mpz_set(private_key_pair.n, n);  
-  gmp_printf("public_key_n is %Zd\n", public_key_pair.n);
-  printf("public_key_e is %lu\n", public_key_pair.e);
-  gmp_printf("private_key_n is %Zd\n", public_key_pair.n);
-  //printf("private_key_d is %lu\n", private_key_pair.d);
+  gmp_printf("private_key_n is %Zd\n", private_key_pair.n);
   gmp_printf("private_key_d is %Zd\n", private_key_pair.d);
 
   mpz_t C; // Ciphertext
   mpz_t M, N; // Cleartext
   mpz_inits(C, M, N, NULL);
 
-  //M = COMPLEX;
   mpz_set_ui(M, 5201314);
-  RSAEncryption(M, public_key_pair.e, public_key_pair.n, &C);
+  RSAEncryption(M, public_key_pair.e, public_key_pair.n, &C); // use rsa algorithm encrypt cleartext M, and then store ciphertext in variable C
   gmp_printf("The Ciphertext is %Zd\n", C);
-  RSADecrypt(C, private_key_pair.d, private_key_pair.n, &N);
+  RSADecrypt(C, private_key_pair.d, private_key_pair.n, &N);  // use rsa algorithm decrypt ciphertext C, and store cleartext in variable N 
   gmp_printf("The Clear text is %Zd\n", N); 
 
   mpz_clear(p);
